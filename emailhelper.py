93878c08049745031b9b2957cc66bb2e1c904fb3
Ravi Aakula <Ravi.Aakula@blackline.com>
2021-01-17 15:04:03 -0800

    more logging

--- emailhelper.py
+++ emailhelper.py
@@ -1,6 +1,8 @@
 
 import smtplib  
+import logging
 def sendmail(subject):
+  try:
         conn =smtplib.SMTP('smtp-mail.outlook.com',587)  
         type(conn)  
         conn.ehlo()  
@@ -8,3 +10,5 @@ def sendmail(subject):
         conn.login('narsagoud.pulla@outlook.com','Automateme@9')  
         conn.sendmail('narsagoud.pulla@outlook.com','urs.ravis@gmail.com',subject)  
         conn.quit() 
+  except Exception as ex:
+          logging.error(ex)
--- login.py
+++ login.py
@@ -1,5 +1,11 @@
 
 
 import emailhelper
+import logging
 
+logging.basicConfig(filename='logs/logfile.log',level=logging.DEBUG,format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
+
+logging.info('Starting process')
 emailhelper.sendmail("login Succeed")
+logging.info('Cmpleted process')
